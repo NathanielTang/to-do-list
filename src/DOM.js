2f25d5bc8f352774fca719812e52bc601b68f7b0
Nathaniel Tang <nathanieltang22@gmail.com>
2022-06-04 16:49:22 -0400

    added logic for creating HTML for tasks

--- src/DOM.js
+++ src/DOM.js
@@ -8,7 +8,43 @@ export function createContent(array) {
     const priority = element.priority;
     const completed = element.completed;
 
+    const taskDiv = document.createElement('div')
+    taskDiv.classList.add('task')
+    content.appendChild(taskDiv)
+
+    const completedSpot = document.createElement('input');
+    completedSpot.setAttribute("type", "checkbox");
+    const titleSpot = document.createElement('p');
+    const descriptionSpot = document.createElement('p');
+    const dueDateSpot = document.createElement('p');
+    const prioritySpot = document.createElement('p');
+    
+    
+    const appendChildArray = [];
+    appendChildArray.push(completedSpot, titleSpot, descriptionSpot, dueDateSpot, prioritySpot )
+    appendChildArray.forEach((element) => {
+        taskDiv.appendChild(element)
+    })
+
+    titleSpot.textContent = title;
+    descriptionSpot.textContent = description;
+    dueDateSpot.textContent = dueDate;
+
+
     console.log(title, description, dueDate, priority, completed)
    });
     
 }
+
+// var checkbox = document.createElement('input');
+// checkbox.type = "checkbox";
+// checkbox.name = "name";
+// checkbox.value = "value";
+// checkbox.id = "id";
+
+// var label = document.createElement('label')
+// label.htmlFor = "id";
+// label.appendChild(document.createTextNode('text for label after checkbox'));
+
+// container.appendChild(checkbox);
+// container.appendChild(label);
--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import { task } from "./task-class";
 import { createContent } from "./DOM";
 
 //test
-const firstTask = new task("aaa", "aaa", "2", "a", "project; m");
-const secondTask = new task("bbb", "bbb", "1", "b", "project; m");
-const thirdTask = new task("ccc", "ccc", "3", "c", "project; m");
-
-
+const firstTask = new task("titleaaa", "descriptionaaa", "02-22-22", "prio", "project; m");
+const secondTask = new task("bbb", "bbb", "01-03-17", "prio", "project; m");
+const thirdTask = new task("ccc", "ccc", "03-12-21", "prio", "project; m");
 
 firstTask.markComplete();
-
 firstTask.toProject("Project: L");
 
-console.log(firstTask);
 //array
+
 const taskArray = [];
 
 taskArray.push(firstTask, secondTask, thirdTask)
@@ -22,10 +19,11 @@ taskArray.push(firstTask, secondTask, thirdTask)
 createContent(taskArray)
 
 
-//task
+//add task
 function addTask(title, description, dueDate, priority, project) {
   //function that uses task class to add new classes
   const newTask = new task(title, description, dueDate, priority, project);
 }
 
 //render
+
