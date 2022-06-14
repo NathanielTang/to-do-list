403d6388bb76a00bca41c8560b4e44afb5a9815d
Nathaniel Tang <nathanieltang22@gmail.com>
2022-06-13 20:38:07 -0400

    changed the default tasks to make sense

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import "./style.css";
 import { Task } from "./task-class";
 import { render, createContent, createSidebar, taskInput } from "./DOM";
-import { Project, addProjectBtn , closePopup} from "./project";
+import { Project, addProjectBtn, closePopup } from "./project";
 import { getToday } from "./date";
 
 //test
 const firstTask = new Task(
-  "titleaaa",
-  "descriptionaaa",
+  "Read",
+  "Continue reading book from chapter ...",
   "02-22-22",
   "prio",
-  "project: m"
+  "Reading"
 );
-const secondTask = new Task("bbb", "bbb", "01-03-17", "prio", "project: 2");
-const thirdTask = new Task("ccc", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas atque voluptatibus error ratione, laborum autem ex tempora fugit. Assumenda dicta ratione vero optio fuga! Officia, odit exercitationem. Expedita culpa, nostrum sunt amet accusamus maxime vitae nulla et recusandae aperiam doloremque eius ab velit tempore quae, illum voluptas deleniti. Cum explicabo perferendis possimus dolore hic nisi deserunt impedit nam maiores autem.", "03-12-21", "prio", "project: d");
+const secondTask = new Task("Gym", "LEG DAY. Hamstring curls; Bulgarian Split Squats (drop set), Romanian Deadlifts, Leg press, Calf Raises", "06-14-22", "prio", "Health");
+const thirdTask = new Task(
+  "Meditate",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas atque voluptatibus error ratione, laborum autem ex tempora fugit. Assumenda dicta ratione vero optio fuga! Officia, odit exercitationem. Expedita culpa, nostrum sunt amet accusamus maxime vitae nulla et recusandae aperiam doloremque eius ab velit tempore quae, illum voluptas deleniti. Cum explicabo perferendis possimus dolore hic nisi deserunt impedit nam maiores autem.",
+  "06-14-22",
+  "prio",
+  "Health"
+);
 
-const defaultProject = new Project("all");
-const firstProject = new Project("project: m");
-const secondProject = new Project("project: 2");
-const thirdProject = new Project("project: d");
+const defaultProject = new Project("All");
+const firstProject = new Project("Reading");
+const secondProject = new Project("Health");
+// const thirdProject = new Project("project: d");
 
 firstTask.markComplete();
 
@@ -29,32 +35,34 @@ const projectArray = [];
 
 taskArray.push(firstTask, secondTask, thirdTask);
 projectArray.push(defaultProject);
-projectArray.push(firstProject, secondProject, thirdProject);
+projectArray.push(firstProject, secondProject);
 
 render(taskArray);
 createSidebar(projectArray);
 
 //add task
-export function addTask(title = "New task", description, dueDate, project = "all") {
+export function addTask(
+  title = "New task",
+  description,
+  dueDate,
+  project = "all"
+) {
   //function that uses task class to add new classes
-  if (dueDate === "" ) {
-      const today = getToday();
-      console.log(today)
-  const newTask = new Task(title, description, today, project); 
-  taskArray.push(newTask);
+  if (dueDate === "") {
+    const today = getToday();
+    console.log(today);
+    const newTask = new Task(title, description, today, project);
+    taskArray.push(newTask);
   } else {
-      const newTask = new Task(title, description, dueDate, project);
-  taskArray.push(newTask);
-    }
-
- 
+    const newTask = new Task(title, description, dueDate, project);
+    taskArray.push(newTask);
+  }
 
   render(taskArray);
   //add new input
   taskInput();
-  
-checkProjectExists(project)
-  
+
+  checkProjectExists(project);
 }
 //add project
 
@@ -72,19 +80,18 @@ export function switchToProject() {
   render(taskArray, name);
 }
 
-
 function checkProjectExists(project) {
-    let check = 'a';
-    projectArray.forEach(Project => {
-        if (Project.name === project) {
-            check = 'b'
-            return
-        } 
-      })
-
-    if (check === 'a') {
-        addProject(project)
+  let check = "a";
+  projectArray.forEach((Project) => {
+    if (Project.name === project) {
+      check = "b";
+      return;
     }
+  });
+
+  if (check === "a") {
+    addProject(project);
+  }
 }
 
 taskInput();
