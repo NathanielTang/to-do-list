1b95f16e89a25c60ec55707593fdd7ae8121a7c3
Nathaniel Tang <nathanieltang22@gmail.com>
2022-06-30 23:34:58 -0400

    .

--- src/DOM.js
+++ src/DOM.js
@@ -1,9 +1,9 @@
-import { switchToProject, addTask } from "./index.js";
+import { switchToProject, addTask } from './index.js';
 
-const content = document.getElementById("content");
+const content = document.getElementById('content');
 
-export function render(array, project = "All") {
-  if (project === "All") {
+export function render(array, project = 'All') {
+  if (project === 'All') {
     createContent(array);
   } else {
     const projectArray = [];
@@ -17,145 +17,135 @@ export function render(array, project = "All") {
   taskInput();
 }
 
-//content
+// content
 export function createContent(array) {
-  content.innerHTML = "";
+  content.innerHTML = '';
 
   array.forEach((element) => {
-    const title = element.title;
-    const description = element.description;
-    const dueDate = element.dueDate;
-    const priority = element.priority;
-    const completed = element.completed;
+    const { title } = element;
+    const { description } = element;
+    const { dueDate } = element;
+    const { priority } = element;
+    const { completed } = element;
 
-    const taskDiv = document.createElement("div");
-    taskDiv.classList.add("task");
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
     content.appendChild(taskDiv);
 
-    const completedSpot = document.createElement("input");
-    completedSpot.setAttribute("type", "checkbox");
-    completedSpot.classList.add('checkbox')
-    const titleSpot = document.createElement("p");
-    const descriptionSpot = document.createElement("textarea");
+    const completedSpot = document.createElement('input');
+    completedSpot.setAttribute('type', 'checkbox');
+    completedSpot.classList.add('checkbox');
+    const titleSpot = document.createElement('p');
+    const descriptionSpot = document.createElement('textarea');
     descriptionSpot.classList.add('description');
-    descriptionSpot.setAttribute("rows", "3");
-    descriptionSpot.setAttribute("cols", "100");
+    descriptionSpot.setAttribute('rows', '3');
+    descriptionSpot.setAttribute('cols', '100');
 
+    const dueDateSpot = document.createElement('p');
+    const prioritySpot = document.createElement('p');
 
-    const dueDateSpot = document.createElement("p");
-    const prioritySpot = document.createElement("p");
-
-    if (completed === "yes") {
+    if (completed === 'yes') {
       completedSpot.checked = true;
     }
 
-    completedSpot.addEventListener("click", () => {
-      if (completed === "yes") {
-        element.completed = "no";
+    completedSpot.addEventListener('click', () => {
+      if (completed === 'yes') {
+        element.completed = 'no';
       }
-      if (completed === "no") {
-        element.completed = "yes";
+      if (completed === 'no') {
+        element.completed = 'yes';
       }
     });
 
-    descriptionSpot.addEventListener('focusout', ()=> {
-        element.description = descriptionSpot.value;
-    })
+    descriptionSpot.addEventListener('focusout', () => {
+      element.description = descriptionSpot.value;
+    });
 
     const appendChildArray = [];
     appendChildArray.push(
       completedSpot,
       titleSpot,
       dueDateSpot,
-      prioritySpot
+      prioritySpot,
     );
     appendChildArray.forEach((element) => {
       taskDiv.appendChild(element);
     });
-    taskDiv.appendChild(descriptionSpot)
+    taskDiv.appendChild(descriptionSpot);
     titleSpot.textContent = title;
     descriptionSpot.textContent = description;
     dueDateSpot.textContent = dueDate;
-
   });
 }
 
-//sidebar
+// sidebar
 
 export function createSidebar(array) {
-  //array of the projects used to create sidebar. Add eventlistener to each div that calls on the
+  // array of the projects used to create sidebar. Add eventlistener to each div that calls on the
   // switchToProject() function.
-  const sidebar = document.getElementById("sidebar-content");
-  sidebar.innerHTML = "";
+  const sidebar = document.getElementById('sidebar-content');
+  sidebar.innerHTML = '';
   array.forEach((element) => {
-    const projectDiv = document.createElement("div");
+    const projectDiv = document.createElement('div');
     projectDiv.textContent = element.name;
-    projectDiv.classList.add("project");
+    projectDiv.classList.add('project');
 
     sidebar.appendChild(projectDiv);
 
-    projectDiv.addEventListener("click", switchToProject);
+    projectDiv.addEventListener('click', switchToProject);
   });
 }
 
-
-//task input
-    //put this at the end of the render function so that the input is at the bottom 
+// task input
+// put this at the end of the render function so that the input is at the bottom
 
 export function taskInput() {
-    const taskInputText = document.createElement('p');
-    taskInputText.textContent = "Add a New Task";
+  const taskInputText = document.createElement('p');
+  taskInputText.textContent = 'Add a New Task';
 
-    const inputDiv = document.createElement('div');
-    inputDiv.classList.add('task', 'inputDiv');
-    content.appendChild(inputDiv);
+  const inputDiv = document.createElement('div');
+  inputDiv.classList.add('task', 'inputDiv');
+  content.appendChild(inputDiv);
 
-    const titleInput = document.createElement('input');
-    titleInput.setAttribute('type', "text");
-    titleInput.id = "titleInput";
-    const titleLabel = document.createElement('label');
-    titleLabel.setAttribute('for', 'titleInput')
-    titleLabel.innerHTML = "Task Name: "
+  const titleInput = document.createElement('input');
+  titleInput.setAttribute('type', 'text');
+  titleInput.id = 'titleInput';
+  const titleLabel = document.createElement('label');
+  titleLabel.setAttribute('for', 'titleInput');
+  titleLabel.innerHTML = 'Task Name: ';
 
+  const descriptionInput = document.createElement('input');
+  descriptionInput.setAttribute('type', 'text');
+  descriptionInput.id = 'descriptionInput';
 
-    const descriptionInput = document.createElement('input');
-    descriptionInput.setAttribute('type', "text");
-    descriptionInput.id = "descriptionInput";
-    
-    const dateInput = document.createElement('input');
-    dateInput.setAttribute('type', "date");
-    dateInput.id = "dateInput";
+  const dateInput = document.createElement('input');
+  dateInput.setAttribute('type', 'date');
+  dateInput.id = 'dateInput';
 
-    const projectInput = document.createElement('input');
-    projectInput.setAttribute('type', "text");
-    projectInput.id = "projectInput";
-    const projectLabel = document.createElement('label');
-    projectLabel.setAttribute('for', 'projectInput');
-    projectLabel.innerHTML = "Project: "
+  const projectInput = document.createElement('input');
+  projectInput.setAttribute('type', 'text');
+  projectInput.id = 'projectInput';
+  const projectLabel = document.createElement('label');
+  projectLabel.setAttribute('for', 'projectInput');
+  projectLabel.innerHTML = 'Project: ';
 
+  const inputBtn = document.createElement('button');
+  inputBtn.id = 'inputBtn';
+  inputBtn.textContent = 'Add Task';
+  inputBtn.addEventListener('click', () => {
+    if (titleInput.value.length <= 0) return;
+    if (projectInput.value.length <= 0) {
+      projectInput.value = 'all';
+    }
+    addTask(titleInput.value, descriptionInput.value, dateInput.value, projectInput.value);
+  });
 
-
-    const inputBtn = document.createElement('button');
-    inputBtn.id = "inputBtn";
-    inputBtn.textContent = 'Add Task';
-    inputBtn.addEventListener('click', () => {
-        if (titleInput.value.length <= 0) return;
-        if (projectInput.value.length <= 0) {
-            projectInput.value = "all";
-        }
-        addTask(titleInput.value, descriptionInput.value, dateInput.value, projectInput.value);
-    })
-
-    inputDiv.appendChild(taskInputText);
-    inputDiv.appendChild(titleLabel);
-    inputDiv.appendChild(titleInput);
-    // inputDiv.appendChild(descriptionInput);
-    inputDiv.appendChild(dateInput);
-    inputDiv.appendChild(projectLabel);
-    inputDiv.appendChild(projectInput);
-    inputDiv.appendChild(inputBtn);
-   
-    
-
+  inputDiv.appendChild(taskInputText);
+  inputDiv.appendChild(titleLabel);
+  inputDiv.appendChild(titleInput);
+  // inputDiv.appendChild(descriptionInput);
+  inputDiv.appendChild(dateInput);
+  inputDiv.appendChild(projectLabel);
+  inputDiv.appendChild(projectInput);
+  inputDiv.appendChild(inputBtn);
 }
-
--- src/index.js
+++ src/index.js
@@ -1,36 +1,38 @@
-import "./style.css";
-import { Task } from "./task-class";
-import { render, createContent, createSidebar, taskInput } from "./DOM";
-import { Project, addProjectBtn, closePopup } from "./project";
-import { getToday } from "./date";
+import './style.css';
+import { Task } from './task-class';
+import {
+  render, createContent, createSidebar, taskInput,
+} from './DOM';
+import { Project, addProjectBtn, closePopup } from './project';
+import { getToday } from './date';
 
-//test
+// test
 const firstTask = new Task(
-  "Read",
-  "Continue reading book from chapter ...",
-  "02-22-22",
-  "Reading"
+  'Read',
+  'Continue reading book from chapter ...',
+  '2022-02-22',
+  'Reading',
 );
-const secondTask = new Task("Gym", "LEG DAY. Hamstring curls; Bulgarian Split Squats (drop set), Romanian Deadlifts, Leg press, Calf Raises", "06-14-22", "Health");
+const secondTask = new Task('Gym', 'LEG DAY. Hamstring curls; Bulgarian Split Squats (drop set), Romanian Deadlifts, Leg press, Calf Raises', '2022-06-14', 'Health');
 const thirdTask = new Task(
-  "Meditate",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas atque voluptatibus error ratione, laborum autem ex tempora fugit. Assumenda dicta ratione vero optio fuga! Officia, odit exercitationem. Expedita culpa, nostrum sunt amet accusamus maxime vitae nulla et recusandae aperiam doloremque eius ab velit tempore quae, illum voluptas deleniti. Cum explicabo perferendis possimus dolore hic nisi deserunt impedit nam maiores autem.",
-  "06-14-22",
-  "Health"
+  'Meditate',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas atque voluptatibus error ratione, laborum autem ex tempora fugit. Assumenda dicta ratione vero optio fuga! Officia, odit exercitationem. Expedita culpa, nostrum sunt amet accusamus maxime vitae nulla et recusandae aperiam doloremque eius ab velit tempore quae, illum voluptas deleniti. Cum explicabo perferendis possimus dolore hic nisi deserunt impedit nam maiores autem.',
+  '2022-06-14',
+  'Health',
 );
-console.log(secondTask.project, firstTask.project, thirdTask.project)
-const defaultProject = new Project("All");
-const firstProject = new Project("Reading");
-const secondProject = new Project("Health");
+console.log(secondTask.project, firstTask.project, thirdTask.project);
+const defaultProject = new Project('All');
+const firstProject = new Project('Reading');
+const secondProject = new Project('Health');
 // const thirdProject = new Project("project: d");
 
 firstTask.markComplete();
 
-//array
+// array
 
 const taskArray = [];
 const projectArray = [];
-let currentProject = "All";
+let currentProject = 'All';
 
 taskArray.push(firstTask, secondTask, thirdTask);
 projectArray.push(defaultProject);
@@ -39,14 +41,14 @@ projectArray.push(firstProject, secondProject);
 render(taskArray);
 createSidebar(projectArray);
 
-//add task
+// add task
 export function addTask(
-  title = "New task",
+  title = 'New task',
   description,
   dueDate,
-  project = "All"
+  project = 'All',
 ) {
-  if (dueDate === "") {
+  if (dueDate === '') {
     const today = getToday();
     console.log(today);
     const newTask = new Task(title, description, today, project);
@@ -57,39 +59,36 @@ export function addTask(
   }
 
   render(taskArray);
-  
- 
 
   checkProjectExists(project);
 }
-//add project
+// add project
 
 export function addProject(name) {
   const newProject = new Project(name);
   projectArray.push(newProject);
-  //dom stuff
+  // dom stuff
   createSidebar(projectArray);
 }
 
-//render
+// render
 
 export function switchToProject() {
   const name = this.textContent;
   render(taskArray, name);
-  //change project to the clicked project
-  currentProject = ""
+  // change project to the clicked project
+  currentProject = '';
 }
 
 function checkProjectExists(project) {
-  let check = "a";
+  let check = 'a';
   projectArray.forEach((Project) => {
     if (Project.name === project) {
-      check = "b";
-      return;
+      check = 'b';
     }
   });
 
-  if (check === "a") {
+  if (check === 'a') {
     addProject(project);
   }
 }
